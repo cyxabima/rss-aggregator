b6c9a658cef37bba77729e3a25a09e61bd6504da
cyxabima <ukashaanwerali@gmail.com>
2025-10-25 19:44:22 +0500

    boilerplate for http server complete

--- main.go
+++ main.go
@@ -1,9 +1,51 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Welcome to Rss Aggregator: -->")
+	godotenv.Load()
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not Found in the environment")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(
+		cors.Options{
+			AllowedOrigins:   []string{"https://*", "http://*"},
+			AllowedMethods:   []string{"GET", "PUT", "POST", "DELETE", "OPTIONS"},
+			AllowedHeaders:   []string{"*"},
+			ExposedHeaders:   []string{"Link"},
+			AllowCredentials: false,
+			MaxAge:           300,
+		}))
+
+	v1Router := chi.NewRouter()
+	router.Mount("/v1", v1Router)
+	v1Router.Get("/healthz", handlerReadinessHandler)
+	v1Router.Get("/err", handlerErrorHandler)
+
+	svr := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+
+	err := svr.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
